7ba9a028c2bdd011a1cfcfde65b17127bbd2a2e1
nguyendangkhuong96 <nguyendangkhuong96@gmail>
2022-08-19 13:56:01 +0700

    fix login

--- src/components/PublicNavbar/index.tsx
+++ src/components/PublicNavbar/index.tsx
@@ -1,59 +1,120 @@
+import Link from 'next/link'
 
 const PublicNavbar = () => {
   return (
-    <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
-      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-          <a className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase" href="/">Rac Roi Shop</a>
-          <button className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
-            <i className="text-white fas fa-bars"></i>
+    <nav className='top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg'>
+      <div className='container px-4 mx-auto flex flex-wrap items-center justify-between'>
+        <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
+          <Link href='/'>
+            <a className='text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase'>
+              Rac Roi Shop
+            </a>
+          </Link>
+          <button
+            className='cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
+            type='button'>
+            <i className='text-white fas fa-bars'></i>
           </button>
         </div>
-        <div className="lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none hidden" id="example-navbar-warning">
-          <ul className="flex flex-col lg:flex-row list-none mr-auto">
-            <li className="flex items-center"><a className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold" href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/overview/notus?ref=nnjs-auth-navbar">
-              <i className="lg:text-blueGray-200 text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2"></i> Docs</a>
+        <div
+          className='lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none hidden'
+          id='example-navbar-warning'>
+          <ul className='flex flex-col lg:flex-row list-none mr-auto'>
+            <li className='flex items-center'>
+              <Link href='/'>
+                <a className='lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold'>
+                  <i className='lg:text-blueGray-200 text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2'></i>{' '}
+                  Docs
+                </a>
+              </Link>
             </li>
           </ul>
-          <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-            <li className="flex items-center">
-              <a className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold" href="#pablo">Demo Pages</a>
-              <div className="hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48">
-                <span className="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400">Admin Layout</span>
-                <a href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Dashboard</a>
-                <a href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Settings</a>
-                <a href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Tables</a>
-                <a href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Maps</a>
-                <div className="h-0 mx-4 my-2 border border-solid border-blueGray-100"></div>
-                <span className="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400">Auth Layout</span>
-                <a href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Login</a>
-                <a href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Register</a>
-                <div className="h-0 mx-4 my-2 border border-solid border-blueGray-100"></div>
-                <span className="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400">No Layout</span>
-                <a href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Landing</a>
-                <a href="#pablo" className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">Profile</a>
+          <ul className='flex flex-col lg:flex-row list-none lg:ml-auto'>
+            <li className='flex items-center'>
+              <Link href='/'>
+                <a className='lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold'>
+                  Demo Pages
+                </a>
+              </Link>
+              <div className='hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48'>
+                <span className='text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400'>
+                  Admin Layout
+                </span>
+                <Link href='/'>
+                  <a className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'>
+                    Dashboard
+                  </a>
+                </Link>
+                <Link href='/'>
+                  <a className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'>
+                    Settings
+                  </a>
+                </Link>
+                <Link href='/'>
+                  <a className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'>
+                    Tables
+                  </a>
+                </Link>
+                <div className='h-0 mx-4 my-2 border border-solid border-blueGray-100'></div>
+                <span className='text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400'>
+                  Auth Layout
+                </span>
+                <Link href='/'>
+                  <a className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'>
+                    Login
+                  </a>
+                </Link>
+                <Link href='/'>
+                  <a className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'>
+                    Register
+                  </a>
+                </Link>
+                <div className='h-0 mx-4 my-2 border border-solid border-blueGray-100'></div>
+                <span className='text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400'>
+                  No Layout
+                </span>
+                <Link href='/'>
+                  <a className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'>
+                    Landing
+                  </a>
+                </Link>
+                <Link href='/'>
+                  <a className='text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700'>
+                    Profile
+                  </a>
+                </Link>
               </div>
             </li>
-            <li className="flex items-center">
-              <a className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-nextjs%2F" target="_blank" rel="noreferrer">
-                <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-facebook text-lg leading-lg "></i>
-                <span className="lg:hidden inline-block ml-2">Share</span>
-              </a>
-
-            </li><li className="flex items-center">
-              <a className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold" href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-nextjs%2F&amp;text=Start%20your%20development%20with%20a%20Free%20Tailwind%20CSS%20and%20NextJS%20UI%20Kit%20and%20Admin.%20Let%20Notus%20NextJS%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level." target="_blank" rel="noreferrer">
-                <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-twitter text-lg leading-lg "></i>
-                <span className="lg:hidden inline-block ml-2">Tweet</span>
-              </a>
+            <li className='flex items-center'>
+              <Link href='/'>
+                <a className='lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold'>
+                  <i className='lg:text-blueGray-200 text-blueGray-400 fab fa-facebook text-lg leading-lg '></i>
+                  <span className='lg:hidden inline-block ml-2'>Share</span>
+                </a>
+              </Link>
             </li>
-            <li className="flex items-center">
-              <a className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold" href="https://github.com/creativetimofficial/notus-nextjs?ref=nnjs-auth-navbar" target="_blank" rel="noreferrer">
-                <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-github text-lg leading-lg "></i>
-                <span className="lg:hidden inline-block ml-2">Star</span>
-              </a></li>
-            <li className="flex items-center">
-              <a className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150" href="https://www.creative-tim.com/product/notus-nextjs?ref=nnjs-auth" target="_blank" rel="noreferrer">
-                <i className="fas fa-arrow-alt-circle-down"></i> Download</a>
+            <li className='flex items-center'>
+              <Link href='/'>
+                <a className='lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold'>
+                  <i className='lg:text-blueGray-200 text-blueGray-400 fab fa-twitter text-lg leading-lg '></i>
+                  <span className='lg:hidden inline-block ml-2'>Tweet</span>
+                </a>
+              </Link>
+            </li>
+            <li className='flex items-center'>
+              <Link href='/'>
+                <a className='lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold'>
+                  <i className='lg:text-blueGray-200 text-blueGray-400 fab fa-github text-lg leading-lg '></i>
+                  <span className='lg:hidden inline-block ml-2'>Star</span>
+                </a>
+              </Link>
+            </li>
+            <li className='flex items-center'>
+              <Link href='/'>
+                <a className='bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150'>
+                  <i className='fas fa-arrow-alt-circle-down'></i> Download
+                </a>
+              </Link>
             </li>
           </ul>
         </div>
