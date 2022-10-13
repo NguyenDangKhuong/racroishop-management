b7d589595e82fad1e1c16f931dbc83eab4061b0d
Khuong <nguyendangkhuong96@gmail.com>
2022-10-14 03:31:25 +0700

    upgrade nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,3 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import Script from 'next/script'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import {
   Hydrate,
@@ -8,12 +5,14 @@ import {
   QueryClientProvider
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Head from 'next/head'
+import Script from 'next/script'
 import { useState } from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import '../styles/globals.css'
 
-function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function App({ Component, pageProps: {  ...pageProps } }: any) {
   const [queryClient] = useState(
     () =>
       new QueryClient({
