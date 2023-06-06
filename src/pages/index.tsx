f6f66a13da4e83656c7b49affe811295c7c8be25
Khuong <nguyendangkhuong96@gmail.com>
2023-06-06 23:06:31 +0700

    apply auth for index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
 import Layout from '../components/Layout'
 
 const Index = () => {
-  return <Layout>loading...</Layout>
+  const { status, data } = useSession()
+  const router = useRouter()
+  useEffect(() => {
+    if (status === 'unauthenticated') router.replace('/signin')
+  }, [status])
+
+  if (status === 'authenticated') {
+    return <Layout>loading...</Layout>
+  }
 }
 
 export default Index
