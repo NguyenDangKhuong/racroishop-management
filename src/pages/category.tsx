a7dddc74c3b09dc1bb86c8bf8f6ae605126dbefe
Khuong <nguyendangkhuong96@gmail.com>
2023-06-08 14:19:31 +0700

    add protected page

--- src/pages/category.tsx
+++ src/pages/category.tsx
@@ -1,16 +1,27 @@
+import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
 import CategoryTable from '../components/CategoryTable'
 import DashboardLayout from '../components/DashboardLayout'
 
 const Category = () => {
-  return (
-    <DashboardLayout>
-      <div className='flex flex-wrap mt-4'>
-        <div className='w-full mb-12 px-4'>
-          <CategoryTable />
+  const { status, data } = useSession()
+  const router = useRouter()
+  useEffect(() => {
+    if (status === 'unauthenticated') router.replace('/signin')
+  }, [status])
+
+  if (status === 'authenticated') {
+    return (
+      <DashboardLayout>
+        <div className='flex flex-wrap mt-4'>
+          <div className='w-full mb-12 px-4'>
+            <CategoryTable />
+          </div>
         </div>
-      </div>
-    </DashboardLayout>
-  )
+      </DashboardLayout>
+    )
+  }
 }
 
 export default Category
--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,16 +1,27 @@
+import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
 import DashboardLayout from '../components/DashboardLayout'
 import OrderTable from '../components/OrderTable'
 
 const ListPage = () => {
-  return (
-    <DashboardLayout>
-      <div className='flex flex-wrap mt-4'>
-        <div className='w-full mb-12 px-4'>
-          <OrderTable />
+  const { status, data } = useSession()
+  const router = useRouter()
+  useEffect(() => {
+    if (status === 'unauthenticated') router.replace('/signin')
+  }, [status])
+
+  if (status === 'authenticated') {
+    return (
+      <DashboardLayout>
+        <div className='flex flex-wrap mt-4'>
+          <div className='w-full mb-12 px-4'>
+            <OrderTable />
+          </div>
         </div>
-      </div>
-    </DashboardLayout>
-  )
+      </DashboardLayout>
+    )
+  }
 }
 
 export default ListPage
