812a05089ce68bf26ffaab6783394f858b24ebec
Khuong <nguyendangkhuong96@gmail.com>
2023-06-26 13:17:14 +0700

    add db check in api func

--- src/pages/api/check-connection.ts
+++ src/pages/api/check-connection.ts
@@ -1,9 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import connectDb from '../../utils/connectDb'
 
-connectDb()
 const checkConnection = (_: NextApiRequest, res: NextApiResponse) => {
-  console.log('Checking connection')
+  connectDb()
   return res.status(200).send('OK')
 }
 
